71e194ed06d16e0e03a4e2aed6f073f92795b406
bryce_000 <bs482@pvsd.org>
2018-09-06 07:10:07 -0400

    added code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	    double d1 = 37.9;
+	    double d2 = 1004.128;
+	    int i1 = 12;
+	    int i2 = 18;
+
+        System.out.println( "Problem 1: " + 57.2*(i1/i2)+1);
+        System.out.println( "Problem 2: " + 57.2*((double)i1/i2)+1);
+        System.out.println( "Problem 3: " + (15+(i1*-1)*(d1*3)+4));
+        System.out.println( "Problem 4: " + (15+(i1*-1)*(int)(d1*3)+4));
+        System.out.println( "Problem 5: " + (15+(i1*-1)*((int)d1*3)+4));
+
+}
 }
